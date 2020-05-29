c9f87a07d49c8750d535ff386a154dc09d25118e
Tyler Firth <tyfirth@gmail.com>
2020-05-29 21:17:17 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -3,3 +3,17 @@
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
 
+class EmailAddressParser
+  
+  attr_accessor :emails
+  
+  def parse
+    
+  end
+  
+  
+  
+  
+  
+  
+end
